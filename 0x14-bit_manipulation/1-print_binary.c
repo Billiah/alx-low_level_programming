91748682632fb66bc4c937a4af992c5da66fc479
Billiah <kn.billiah@gmail.com>
2023-05-05 05:11:56 -0700

    print_binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/*
+ *print_binary- prints the binary representation of a number
+ *@n: integer variable for the number to be converted
+ *
+ * Return: void
+ */
+void print_binary(unsigned long int n)
+{
+
+	if (n > 1)
+	{
+		print_binary(n >> 1);
+	}
+	else if (n & 1)
+	{
+		_putchar('1');
+	}
+	else
+	{
+		_putchar('0');
+	}
+}
+
